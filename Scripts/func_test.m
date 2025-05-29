ad1ebe5f6422fcc87e4dcfd3b694e78539ee788d
Zewen Kong <zewen.kong@outlook.com>
2025-05-29 17:32:03 +0100

    style: code format

--- Scripts/func_test.m
+++ Scripts/func_test.m
@@ -2,14 +2,14 @@
 % -
 % - func. >> ionic charge v.s. time
 % - parad. >> dfplot_ionic.Qt_ionic(sol, x1, x2);
-% -
+
 dfplot_ionic.Qt_ionic(sol, 0, sol.x(:, end));
 
 %% - - - - - - - - - -
 % -
 % - func. >> species current at special position point
 % - parad. >> dfplot_ionic.Jt_species(sol, position);
-% -
+
 dfplot_ionic.Jt_species(sol, sol.x(:, end));
 
 %% - - - - - - - - - -
@@ -19,41 +19,41 @@ dfplot_ionic.Jt_species(sol, sol.x(:, end));
 % -
 dfplot.Jx(sol);
 
-% ############################################################
-% ### Energy level at special time points ####################
-% ############################################################
-%
+% ============================================================
+% === Energy level at special time points                    =
+% ============================================================
+
 %% - - - - - - - - - -
 % -
 % - func. >> plot eng lvl diagram for special current fluxes
-% - parad. >> N/A
 % -
-[t_J_max, t_J_corr] = dfana_ionic.tPntOfSpJs(sol, 0); % dfana_ionic.m
-%
-%% - - - - - - - - - - energy level diagram at special time point
+[t_J_max, t_J_corr] = dfana_ionic.tPntOfSpJs(sol, 0);
+
+%% - - - - - - - - - -
+% -
+% - func. >> energy level diagram at special time point
 % -
 dfplot.ELx(sol, [t_J_max, t_J_corr]);
-% dfplot.ELx(sol, t_J_corr);
 
-%dfplot.ELxnpxacx(sol, t_J_max);
-%dfplot.ELxnpxacx(sol, t_J_corr);
-%
 %% - - - - - - - - - -
 % -
 idx_t_J_max = find(t == t_J_max);
 idx_t_J_corr = find(t == t_J_corr);
 
-% ############################################################
-% ### Special point as new equilibrate solution  #############
-% ############################################################
-%
+% ============================================================
+
+% ============================================================
+% === Special point as new equilibrate solution              =
+% ============================================================
+
 %% - - - - - - - - - -
 % -
+% - func. >> generate equilibrate solution of special points and do CV
+% -
 [soleq_1, soleq_2] = dfana_ionic.spPntTosoleq(sol, 0);
-
 sol_1 = doCV(soleq_1, 0, 0, -1, 1, 1e-1, 1, 500);
 sol_2 = doCV(soleq_2, 0, 0, -1, 1, 1e-1, 1, 500);
-%
+
 %% - - - - - - - - - -
 % -
 J_1 = dfana.calcJ(sol_1);
@@ -65,11 +65,12 @@ ppos_1 = getpointpos(xpos, xmesh);
 
 figure('Name', 'Special point as equilibrium solution')
 plot(Vapp_1, J_1.tot(:, ppos_1), 'LineWidth', 1);
-%% -
+
+%% - - - - - - - - - -
+% -
 dfplot.ELx(sol_1); % energy level plot
-%% -
-% dfplot_ionic.Qt_ionic(sol_1, 0, sol_1.x(:, end)); % charge
-%
+dfplot_ionic.Qt_ionic(sol_1, 0, sol_1.x(:, end)); % charge
+
 %% - - - - - - - - - -
 % -
 J_2 = dfana.calcJ(sol_2);
@@ -81,7 +82,13 @@ ppos_2 = getpointpos(xpos, xmesh);
 
 figure('Name', 'Special point as equilibrium solution')
 plot(Vapp_2, J_2.tot(:, ppos_2), 'LineWidth', 1);
-%% -
-dfplot.ELx(sol_2); % energy level plot
-%% -
+
+%% - - - - - - - - - -
+% -
+dfplot.ELx(sol_2);
 dfplot_ionic.Qt_ionic(sol_2, 0, sol_2.x(:, end));
+
+% ============================================================
+
+% one layer with both silver electrode (w/ or w/o BV)
+% const Vset and V read pulse
--- initialise_df.m
+++ initialise_df.m
@@ -1,32 +1,36 @@
 function initialise_df
-%
-%% LICENSE
-% Copyright (C) 2020  Philip Calado, Ilario Gelmetti, and Piers R. F. Barnes
-% Imperial College London
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Affero General Public License as published
-% by the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-%% Start code
-% Plotting defaults
-set(0,'DefaultLineLinewidth',1.5);
-set(0,'DefaultAxesFontSize',16);
-set(0,'DefaultFigurePosition', [600, 400, 600, 450]);
-set(0,'DefaultAxesXcolor', [0, 0, 0]);
-set(0,'DefaultAxesYcolor', [0, 0, 0]);
-set(0,'DefaultAxesZcolor', [0, 0, 0]);
-set(0,'DefaultTextColor', [0, 0, 0]);
+    %
+    % LICENSE
+    % Copyright (C) 2020  Philip Calado, Ilario Gelmetti, and Piers R. F. Barnes
+    % Imperial College London
+    % This program is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU Affero General Public License as published
+    % by the Free Software Foundation, either version 3 of the License, or
+    % (at your option) any later version.
+    %
+    %% - - - - - - - - - - CODE START - - - - - - - - - -
 
-% Add file paths to functions, avoid hidden directories like .git
-addpath(pwd)
-files = dir(pwd);
-dirs = files(cat(1,files.isdir));
-for i=1:length(dirs)
-    dirname = dirs(i).name;
-    if ~any(regexp(dirname,'^\.'))
-        addpath(dirname);
+    % - - - - - - - - - - plotting defaults
+    set(0, 'DefaultLineLinewidth', 1.5);
+    set(0, 'DefaultAxesFontSize', 16);
+    set(0, 'DefaultFigurePosition', [600, 400, 600, 450]);
+    set(0, 'DefaultAxesXcolor', [0, 0, 0]);
+    set(0, 'DefaultAxesYcolor', [0, 0, 0]);
+    set(0, 'DefaultAxesZcolor', [0, 0, 0]);
+    set(0, 'DefaultTextColor', [0, 0, 0]);
+
+    % - - - - - - - - - - add file paths to functions, avoid hidden directories like .git
+    addpath(pwd)
+    files = dir(pwd);
+    dirs = files(cat(1, files.isdir));
+
+    for i = 1:length(dirs)
+        dirname = dirs(i).name;
+
+        if ~any(regexp(dirname, '^\.'))
+            addpath(dirname);
+        end
+
     end
-end
 
 end
