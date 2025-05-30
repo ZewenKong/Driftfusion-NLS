9bcb69216bb228c86c34be4d7f3abb44a31e9067
Zewen Kong <zewen.kong@outlook.com>
2025-05-30 17:56:33 +0100

    style: files organisation

--- Scripts/main.m
+++ Scripts/main.m
@@ -1,25 +1,12 @@
-
 % - - - - - - - - - - initialisation
 equilibrate_init();
 initialise_df;
 
-% C2C
-% >> ON/OFF ratio; SET/REST voltage
-%
-% Endurance test
-% >> C2C
+% - - - - - - - - - - C2C
+% ON/OFF ratio; SET/REST voltage
+% Endurance test; Stochasticity/Variability test
+C2C;
 
+% - - - - - - - - - - D2D_var
 % Stochasticity/Variability test
-% >> C2C; D2D
-%
-% Retention time
-% >> R - t, measures long-term retention stability over time
-% >> I - t, monitors current decay over time to check for charge leakage
-%
-% Power consumption
-%
-% Switching speed
-% >> R - t, tracks resistance change
-% >> I - t, measures transition time
-
-
+D2D_device;
