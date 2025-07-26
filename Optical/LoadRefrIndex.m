0f934d26304b209c364ea422b8a42c730ce7c381
Zewen Kong <zewen.kong@outlook.com>
2025-07-26 13:20:05 +0100

    250726 - doc: format

--- Optical/LoadRefrIndex.m
+++ Optical/LoadRefrIndex.m
@@ -1,25 +1,21 @@
-% Function LoadRefrIndex
-% This function returns the complex index of refraction spectra, ntotal, for the
-% material called 'name' for each wavelength value in the wavelength vector
-% 'wavelengths'.  The material must be present in the index of refraction
-% library 'Index_of_Refraction_library.xls'.  The program uses linear
-% interpolation/extrapolation to determine the index of refraction for
-% wavelengths not listed in the library.
-function [n_interp, k_interp] = LoadRefrIndex(name,wavelengths)
+function [n_interp, k_interp] = LoadRefrIndex(name, wavelengths)
+    % This function returns the complex index of refraction spectra, ntotal, for the
+    % material called 'name' for each wavelength value in the wavelength vector
+    % 'wavelengths'.  The material must be present in the index of refraction
+    % library 'Index_of_Refraction_library.xls'.  The program uses linear
+    % interpolation/extrapolation to determine the index of refraction for
+    % wavelengths not listed in the library.
 
-%Data in IndRefr
-IndRefr=readtable('Index_of_Refraction_library.xls', 'VariableNamingRule', 'preserve');
+    % data in IndRefr
+    IndRefr = readtable('Index_of_Refraction_library.xls', 'VariableNamingRule', 'preserve');
 
-% Load index of refraction data in spread sheet, will crash if misspelled
-file_wavelengths=IndRefr.(1);
-n=IndRefr.(strcat(name,'_n'));
-k=IndRefr.(strcat(name,'_k'));
+    % load index of refraction data in spread sheet, will crash if misspelled
+    file_wavelengths = IndRefr.(1);
+    n = IndRefr.(strcat(name, '_n'));
+    k = IndRefr.(strcat(name, '_k'));
 
-% Interpolate/Extrapolate data linearly to desired wavelengths
-n_interp=interp1(file_wavelengths, n, wavelengths, 'linear', 'extrap');
-k_interp=interp1(file_wavelengths, k, wavelengths, 'linear', 'extrap');
-
-%Return interpolated complex index of refraction data
-%ntotal = n_interp+1i*k_interp; 
+    % interpolate/Extrapolate data linearly to desired wavelengths
+    n_interp = interp1(file_wavelengths, n, wavelengths, 'linear', 'extrap');
+    k_interp = interp1(file_wavelengths, k, wavelengths, 'linear', 'extrap');
 
 end
